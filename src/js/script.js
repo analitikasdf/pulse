e4bbc7720422a75f92c624f11ea5798345c7e4f0
analitikasdf <analitik.asdf@gmail.com>
2020-07-01 12:36:11 +0300

    01.07.2020

--- src/js/script.js
+++ src/js/script.js
@@ -13,23 +13,25 @@ $(document).ready(function(){
             arrows: false,
           }
         },
-        // {
-        //   breakpoint: 575,
-        //   settings: {
-        //     dots: true,
-        //     arrows: false,
-        //   }
-        // },
-        // {
-        //   breakpoint: 320,
-        //   settings: {
-        //     dots: true,
-        //     arrows: false,
-        //   }
-        // }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide (item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+    }
+
+    toggleSlide ('.catalog-item__link');
+    toggleSlide ('.catalog-item__back');
   });
