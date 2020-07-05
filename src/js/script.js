e2dae4e25335ee2e1cc7886308fd7cc08ced5617
analitikasdf <analitik.asdf@gmail.com>
2020-07-05 16:34:42 +0300

    05.07.2020

--- src/js/script.js
+++ src/js/script.js
@@ -50,5 +50,92 @@ $(document).ready(function(){
         $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('slow');
       });
-    })
+    });
+
+    function validateForm (form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true,
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите своё имя!",
+            minlength: jQuery.validator.format("Введите больше {0} символов!")
+          },
+          phone: "Пожалуйста, введите номер телефона!",
+          email: {
+            required: "Пожалуйста, введите адрес вашей электронной почты!",
+            email: "Введите адрес в формате: name@domain.com"
+          }
+        },
+      });
+    };
+
+    validateForm('#consultation form');
+    validateForm('#order form');
+	validateForm('#consultation-form');
+	
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut('slow');
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
   });
+
+  // smooth scroll and page up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn('slow');
+    } else {
+      $('.pageup').fadeOut('slow');
+    };
+  });
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
+});
